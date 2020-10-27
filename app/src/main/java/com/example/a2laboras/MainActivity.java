d4d7dcb051c0fbdbdc9e6a1cb4c21866e4613182
shermonas <viliuunas@gmail.com>
2020-10-27 16:55:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a2laboras/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a2laboras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spCountingOption;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.spCountingOption  = findViewById(R.id.spCountingOption );
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.selection_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spCountingOption.setAdapter(adapter);
+
+    }
+
+    public void onBtnShowResultClick(View view) {
+    }
+}
